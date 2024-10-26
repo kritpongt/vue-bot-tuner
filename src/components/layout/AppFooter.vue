e0187e12cc0162d88b5b42c449f707f75d9007bc
kpt1p <krittapongtip@gmail.com>
2024-10-26 17:49:34 +0700

    create footer layout

---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div class="flex justify-center items-center py-4 gap-2">
+        Bot Tuner by<a href="#" rel="" class="">kpt1p</a>
+    </div>
+</template>
